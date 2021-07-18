775c57891cdc4271e706de3fc364a61558f93a09
Keshav-WhiteHat <79103641+Keshav-WhiteHat@users.noreply.github.com>
2021-07-18 12:51:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+var mouseEvent = "empty";
+
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 1;
+    canvas.addEventListener("mousedown", my_mousedown);
+    function my_mousedown(e)
+    {
+        //Addictonal Activity start
+        radius=document.getElementById("radius").value;
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width").value;
+        //Addictonal Activity ends
+
+        mouseEvent = "mouseDown";
+    }
+
+    canvas.addEventListener("mousemove", my_mousemove);
+    function my_mousemove(e)
+    {
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+        if (mouseEvent == "mouseDown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+       ctx.arc( current_position_of_mouse_x, current_position_of_mouse_y,radius,0,2*Math.PI);
+        ctx.stroke();
+        }
+
+      
+    }
+
+    canvas.addEventListener("mouseup", my_mouseup);
+    function my_mouseup(e)
+    {
+        mouseEvent = "mouseUP";
+    }
+
+    canvas.addEventListener("mouseleave", my_mouseleave);
+    function my_mouseleave(e)
+    {
+        mouseEvent = "mouseleave";
+    }
+
+
+//Additional Activity
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
